a21829c83019723175c5f6c2591bc7201da133aa
Achegnui Juven <achegnuijuven@gmail.com>
2023-08-04 09:34:35 +0100

    Corrected

--- market_list.js
+++ market_list.js
@@ -1,9 +1,8 @@
 let myArray=[['Fish', 100], ['meat', 200], ['gnut', 100]];
 
 //Function to delete item
-function delItem(myArray, itemdel){
+function delItem(itemdel){
     for(let i=0; i<myArray.length; i++){
-        let item = myArray[i];
         if(myArray[i][0] === itemdel){
             myArray.splice(i--, 1);
             break;
@@ -12,26 +11,29 @@ function delItem(myArray, itemdel){
     return myArray;
 }
 
-console.log(delItem(myArray, 'Fish'));
+
 
 
  //function to add items
-function addItems(myArray, elt, price){
+function addItems(elt, price){
     let new_item = [elt, price];
     myArray.push(new_item);
     return myArray;
 }
 
-console.log(addItems(myArray, 'garri', 200));
+
  
 //function to add prices
-function addPrices(myArray){
+function addPrices(){
     let sum = 0;
     for(let i=0; i<myArray.length; i++){
-        let item = myArray[i];
         sum += myArray[i][1];
      }
     return sum;
 }
 
-console.log(addPrices(myArray))
+console.log(addItems('garri', 200))
+
+console.log(delItem('Fish'))
+
+console.log(addPrices())
